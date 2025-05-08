86738c07da10e0a0c408e3828f996ccb5b9469a1
MinhHoang1225 <minhhoang121225@gmail.com>
2025-05-08 22:31:19 +0700

    Hoang | doing Admin interface

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 
 import AdminDashboard from './pages/Admin/AdminDashboard';
 import AdminUser from './pages/Admin/AdminUser';
+import Login from './pages/Login/login';
+
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
 
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
         <Route path="/admin-user" element={<AdminUser />} />
+        <Route path="/login" element={<Login />} />
+
 
 
         <Route path="*" element={<h1>404 - Page Not Found</h1>} />
