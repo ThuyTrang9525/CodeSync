07cd4ff3e2f97f7ca57d9cd1f79dbbc80724f2ee
Hasangnguyen <sanghanguyen05@gmail.com>
2025-05-09 20:10:48 +0700

    add profile

---
+++ src/components/Student/uploadAchivementModal.js
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+import "../assets/css/uploadAchivement.css";
+
+
+Modal.setAppElement('#root');
+
+
+export default function UploadAchievementsModal({ isOpen, onRequestClose }) {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      contentLabel="Upload Achievements"
+      className="achievement-modal"
+      overlayClassName="custom-overlay" // Đã đổi tên tại đây
+    >
+      <h2 className="modal-title">Upload Achievements</h2>
+      <hr className="divider" />
+      <div className="icon-wrapper">
+        <span className="icon">🏆</span>
+      </div>
+      <form className="achievement-form">
+        <div className="form-group">
+          <label htmlFor="achievementName">Name of achievement</label>
+          <input
+            type="text"
+            id="achievementName"
+            placeholder="Enter achievement name"
+          />
+        </div>
+
+
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              placeholder="Enter description"
+            ></textarea>
+        </div>
+
+
+        <div className="form-group">
+          <label htmlFor="fileUpload">Image / PDF / Document</label>
+          <input
+            type="file"
+            id="fileUpload"
+            onChange={handleFileChange}
+          />
+        </div>
+
+
+        <div className="button-group">
+          <button
+            type="button"
+            onClick={onRequestClose}
+            className="cancel-button"
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="save-button"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
+}
