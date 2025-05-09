f03b1ed49654ac3e8a8988cd7f653941f1a988ba
Hasangnguyen <sanghanguyen05@gmail.com>
2025-05-09 22:05:19 +0700

    interface the homepage of student

--- src/pages/student/StudentManagementStudy.js
+++ src/pages/student/StudentManagementStudy.js
@@ -1,13 +1,11 @@
 import StudyPlanManagement from "../../components/Student/study-plan-management"
-import "../../css/student-management-study.css";
+import "../../assets/css/student-management-study.css";
 import Header from "../../components/header"
 import Footer from "../../components/footer"
 export default function StudentManagementStudy() {
   return (
     <main className="container mx-auto p-4">
-       <Header />
        <StudyPlanManagement />
-        <Footer />
     </main>
   )
 }
